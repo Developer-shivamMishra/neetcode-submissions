a7ff1b28249b3cdc4a8c49be7b67bb1cee15bb8d
Shivam Mishra <sm2275470@gmail.com>
2026-09-22 16:13:53 -0400

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        int fre = 0; int ans = 0;
+        for(int i=0; i<n ; i++){
+
+            if(fre == 0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                fre++;
+            }else{
+                fre--;
+            }
+        }
+
+        return ans;
+
+    }
+};
